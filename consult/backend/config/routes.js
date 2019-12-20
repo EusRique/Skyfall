4962256543ef2f933225b999c5078e50431870f7
Henrique <hiquesc_@hotmail.com>
2019-12-20 08:50:54 -0300

    validando usuario administrador

--- consult/backend/config/routes.js
+++ consult/backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
@@ -5,20 +7,20 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
     
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
     
     app.route('/client_analysis')
         .all(app.config.passport.authenticate())
-        .get(app.api.client_analysis.get)
-        .post(app.api.client_analysis.save)
+        .get(admin(app.api.client_analysis.get))
+        .post(admin(app.api.client_analysis.save))
     
     app.route('/client_analysis/:id')
         .all(app.config.passport.authenticate())    
-        .get(app.api.client_analysis.getById)
+        .get(admin(app.api.client_analysis.getById))
 }
