6f98ef9320744679b277e3117c447318df1fa11e
Henrique <hiquesc_@hotmail.com>
2019-12-20 08:35:00 -0300

    autorização para acesso as api

--- consult/backend/config/routes.js
+++ consult/backend/config/routes.js
@@ -4,17 +4,21 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
     
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
     
     app.route('/client_analysis')
+        .all(app.config.passport.authenticate())
         .get(app.api.client_analysis.get)
         .post(app.api.client_analysis.save)
     
     app.route('/client_analysis/:id')
+        .all(app.config.passport.authenticate())    
         .get(app.api.client_analysis.getById)
 }
