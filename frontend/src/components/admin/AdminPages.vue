2e6be201576ca79fcb83dcffa0e8d277b45f393c
Henrique <hiquesc_@hotmail.com>
2019-12-21 17:49:45 -0300

    Router

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
+            sub="Controle de Usuários" />
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle }    
+}
+</script>
---
+++ frontend/src/components/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+        <PageTitle icon="fa fa-home" main="Dashboard"
+            sub="Analise de Crédito" />
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'Home',
+    components: { PageTitle }    
+}
+</script>
