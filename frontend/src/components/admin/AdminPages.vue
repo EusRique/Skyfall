0f7845bcd5331bf13530b6c3326181fa30dcff97
Henrique <hiquesc_@hotmail.com>
2019-12-21 18:56:57 -0300

    Paginas administrativas

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
-            sub="Controle de Usuários" />
+            sub="Cadastros & Cia" />
+        
+        <div class="admin-pages-tabs">
+            <b-card no body>
+                <b-tabs card>
+                    <b-tab title="Consultar" active>
+                        <ConsultAdmin />
+                    </b-tab>
+                    <b-tab title="Usuários">
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ConsultAdmin from './ConsultAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle }    
+    components: { PageTitle, ConsultAdmin, UserAdmin }    
 }
 </script>
---
+++ frontend/src/components/admin/ConsultAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="consult-admin">
+        <h1>Consultar CPF</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ConsultAdmin'
+}
+</script>
+
+<style>
+    
+</style>
