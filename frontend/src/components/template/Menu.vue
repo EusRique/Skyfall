1790168de28509bbf007a744e9a34e806ec7f608
Henrique <hiquesc_@hotmail.com>
2019-12-21 12:57:29 -0300

    Estrutura inicial template

---
+++ frontend/src/components/template/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <aside class="footer">
+
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style>
+    .menu {
+        grid-area: menu;
+        background: linear-gradient(to right, #232526, #414345);
+        display: flex;
+        flex-direction: columns;
+        flex-wrap: wrap;
+    }
+</style>
