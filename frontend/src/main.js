2ff3ef1d4baf7437fd76b505b8c7c383fc06abe6
Henrique <hiquesc_@hotmail.com>
2019-12-21 19:59:18 -0300

    Cadastro de usuário

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 //TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkhlbnJpcXVlIiwiZW1haWwiOiJoZW5yaXF1ZUBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTc2ODQ1NzE1LCJleHAiOjE1NzcxMDQ5MTV9.VZvPrISfrm5naB53Og04BvNTG_Mze7x11ZaCd1kKwsw'
+
 new Vue({
   store,
   router,
