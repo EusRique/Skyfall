afb7a15dca1b3e5485ce18e68ef1fb382c034a8f
Henrique <hiquesc_@hotmail.com>
2019-12-22 20:26:44 -0300

    estrutura para busca por cpf

--- frontend/src/components/home/Consult.vue
+++ frontend/src/components/home/Consult.vue
@@ -1,12 +1,44 @@
 <template>
     <div class="consult-admin">
         <h1>Consultar CPF</h1>
+        <b-table hover striped @click="selectedCustomer" :items="customer" :fields="fields">
+            
+        </b-table>
     </div>
 </template>
 
 <script>
+import { baseApiUrl, showError } from '@/global'
+import axios from 'axios'
+
 export default {
-    name: 'Consul'
+    name: 'Consul',
+    data: function() {
+        return {
+            customer: [],
+            fields: [
+                { key: 'id', label: 'Código', sortable: true },
+                { key: 'name', label: ':Nome', sortable: true },
+                { key: 'address', label: 'Endereço', sortable: true },
+                { key: 'cpf', label: 'CPF', sortable: true},
+                { key: 'age', label: 'Idade' }
+            ]
+        }
+    },
+    methods: {
+        loadCustomer() {
+            const url = `${baseApiUrl}/client_analysis`
+            axios.get(url).then(res => {
+                this.customer = res.data
+            })
+        },
+        selectedCustomer() {
+            console.log("Aqui")
+        }
+    },
+    mounted() {
+        this.loadCustomer()
+    }
 }
 </script>
 
